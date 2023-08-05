9a04a2755e4691de3bf2566a809e06892a16bd57
acwstudio <acwstudio2018@gmail.com>
2023-08-06 01:00:34 +0300

    vv

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+import './bootstrap.js';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
+
+InertiaProgress.init({ color: '#4B5563' });
