8eeced771524d88ed83e5d683efec9a90eeeed48
acwstudio <acwstudio2018@gmail.com>
2023-08-08 03:52:36 +0300

    added user email verification

--- src/Domain/Shared/Models/User.php
+++ src/Domain/Shared/Models/User.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Domain\Shared\Models;
 
 use Database\Factories\Shared\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-final class User extends Authenticatable
+final class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
